19cb1a842b8e0d31ba3bbac1587d4653d466806b
gaelmos <156100424+gaelmos@users.noreply.github.com>
2024-06-24 15:57:46 -0300

    objetos

---
+++ Assets/moverobjetos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverobjetos : MonoBehaviour
+{
+    public GameObject[] objetos;
+    public int valor = 0;
+
+    void Start()
+    {
+        objetosborrados();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+         {
+            valor++;
+            if(valor >= objetos.Length)
+            {
+                valor = 0;
+
+            }
+            objetosborrados();
+            activarobjetos(valor);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            valor--;
+            if (valor >= objetos.Length)
+            {
+                valor = 0;
+
+            }
+            objetosborrados();
+            activarobjetos(valor);
+        }
+    }
+    void objetosborrados()
+    {
+        for(int i=0; i<objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+    void activarobjetos(int index)
+    {
+        objetos[index].SetActive(true);
+    }
+}
